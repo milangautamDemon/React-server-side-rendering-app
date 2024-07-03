6c5f5291acd7c428987aad5c33ca6399e7afb1c5
Milan Gautam <milangautam071440304@gmail.com>
2024-07-03 12:52:28 +0545

    connection to dom-root & app using hydrateRoot for ssr

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM, { hydrateRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
+/* 
+//client-side rendering
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+*/
+
+//server-side rendering
+const domRootContainer = document.getElementById("root");
+hydrateRoot(container, <App />);
